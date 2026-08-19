d5c7a962e43ca77adea5ad5a520978eb4a83edfe
Murad Hasan <mdmuradhasan151@gmail.com>
2026-08-20 02:57:48 +0600

    add .env file and hide firebase credentials

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAFDreBlxGreRD5xLUpH7zuTcQFvnj2Irc",
-    authDomain: "smart-deals-bcc99.firebaseapp.com",
-    projectId: "smart-deals-bcc99",
-    storageBucket: "smart-deals-bcc99.firebasestorage.app",
-    messagingSenderId: "1036819681328",
-    appId: "1:1036819681328:web:484da89b69f13c6ff49df5"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId
 };
 
 const app = initializeApp(firebaseConfig);
